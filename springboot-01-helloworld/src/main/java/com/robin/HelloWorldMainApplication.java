4280aa3199a0ea75546e783571511bdf1027d0f6
robin-qu <hongbinqu9@gmail.com>
2020-08-23 15:34:11 +0800

    hello springboot

--- springboot-01-helloworld/src/main/java/com/robin/HelloWorldMainApplication.java
+++ springboot-01-helloworld/src/main/java/com/robin/HelloWorldMainApplication.java
@@ -1,4 +1,15 @@
 package com.robin;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @SpringBootApplication: declares this is an Spring Boot application
+ */
+@SpringBootApplication
 public class HelloWorldMainApplication {
+    public static void main(String[] args) {
+        // launch the spring application
+        SpringApplication.run(HelloWorldMainApplication.class, args);
+    }
 }
