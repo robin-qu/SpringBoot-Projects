38368272ba2c2034c789595b15ee40481b19732c
robin-qu <hongbinqu9@gmail.com>
2020-08-28 23:32:12 +0800

    hello springboot detail

--- springboot-01-helloworld/src/main/java/com/robin/controller/HelloController.java
+++ springboot-01-helloworld/src/main/java/com/robin/controller/HelloController.java
@@ -3,10 +3,12 @@ package com.robin.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+//@ResponseBody
+@RestController
 public class HelloController {
-    @ResponseBody
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World!";
