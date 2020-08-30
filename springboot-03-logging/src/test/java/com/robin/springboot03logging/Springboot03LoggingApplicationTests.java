efd0fe6b776992d14236cdb51129c199ac74aeae
robin-qu <hongbinqu9@gmail.com>
2020-08-30 19:22:24 +0800

    springboot logging

--- springboot-03-logging/src/test/java/com/robin/springboot03logging/Springboot03LoggingApplicationTests.java
+++ springboot-03-logging/src/test/java/com/robin/springboot03logging/Springboot03LoggingApplicationTests.java
@@ -1,13 +1,20 @@
 package com.robin.springboot03logging;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Springboot03LoggingApplicationTests {
+	Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Test
 	void contextLoads() {
+		logger.trace("This is trace log...");
+		logger.debug("This is debug log...");
+		logger.info("This is info log...");
+		logger.warn("This is warning log...");
+		logger.error("This is error log...");
 	}
-
 }
