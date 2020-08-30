370a0d84e3d883ff0ccfe4f7378051a29720834f
robin-qu <hongbinqu9@gmail.com>
2020-08-30 18:16:59 +0800

    springboot logging initial commit

---
+++ springboot-03-logging/src/test/java/com/robin/springboot03logging/Springboot03LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robin.springboot03logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot03LoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
