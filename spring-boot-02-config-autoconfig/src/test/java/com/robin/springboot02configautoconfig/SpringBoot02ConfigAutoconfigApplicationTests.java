bb863fada8a57a9f18a260e8f68a69d3a432ed6d
robin-qu <hongbinqu9@gmail.com>
2020-08-30 17:25:04 +0800

    springboot autoconfig initial commit

---
+++ spring-boot-02-config-autoconfig/src/test/java/com/robin/springboot02configautoconfig/SpringBoot02ConfigAutoconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robin.springboot02configautoconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot02ConfigAutoconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
