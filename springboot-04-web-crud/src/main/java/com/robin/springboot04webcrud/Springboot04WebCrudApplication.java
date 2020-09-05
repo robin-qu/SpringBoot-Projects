e02b9d81cbe8a25cc7d262ad5a66410778525f47
robin-qu <hongbinqu9@gmail.com>
2020-09-05 17:49:20 +0800

    springboot web initial commit

---
+++ springboot-04-web-crud/src/main/java/com/robin/springboot04webcrud/Springboot04WebCrudApplication.java
@@ -0,0 +1,13 @@
+package com.robin.springboot04webcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot04WebCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot04WebCrudApplication.class, args);
+    }
+
+}
---
+++ springboot-04-web-crud/src/test/java/com/robin/springboot04webcrud/Springboot04WebCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robin.springboot04webcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot04WebCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
