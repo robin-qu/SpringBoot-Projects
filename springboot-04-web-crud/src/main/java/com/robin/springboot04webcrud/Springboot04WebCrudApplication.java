5d539657a8b88e933bebe759c5dfe4b8f9218a06
robin-qu <hongbinqu9@gmail.com>
2020-09-12 16:56:53 +0800

    springboot mvc

--- springboot-04-web-crud/src/main/java/com/robin/springboot04webcrud/Springboot04WebCrudApplication.java
+++ springboot-04-web-crud/src/main/java/com/robin/springboot04webcrud/Springboot04WebCrudApplication.java
@@ -2,6 +2,11 @@ package com.robin.springboot04webcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class Springboot04WebCrudApplication {
@@ -10,4 +15,15 @@ public class Springboot04WebCrudApplication {
         SpringApplication.run(Springboot04WebCrudApplication.class, args);
     }
 
+    @Bean
+    public ViewResolver myViewResolver() {
+        return new MyViewResolver();
+    }
+
+    private static class MyViewResolver implements ViewResolver {
+        @Override
+        public View resolveViewName(String s, Locale locale) throws Exception {
+            return null;
+        }
+    }
 }
