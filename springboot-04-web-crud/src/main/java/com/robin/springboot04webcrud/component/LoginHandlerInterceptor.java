dcd334525c014797664aa87e8a15ea930cacf987
robin-qu <hongbinqu9@gmail.com>
2020-09-12 19:03:30 +0800

    log in page

---
+++ springboot-04-web-crud/src/main/java/com/robin/springboot04webcrud/component/LoginHandlerInterceptor.java
@@ -0,0 +1,30 @@
+package com.robin.springboot04webcrud.component;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginHandlerInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object username = request.getSession().getAttribute("loginuser");
+        if (username == null) {
+            request.setAttribute("msg", "Not authorized, please log in.");
+            request.getRequestDispatcher("/index.html").forward(request, response);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
--- springboot-04-web-crud/src/main/java/com/robin/springboot04webcrud/config/MyMvcConfig.java
+++ springboot-04-web-crud/src/main/java/com/robin/springboot04webcrud/config/MyMvcConfig.java
@@ -1,8 +1,10 @@
 package com.robin.springboot04webcrud.config;
 
+import com.robin.springboot04webcrud.component.LoginHandlerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,6 +16,12 @@ public class MyMvcConfig implements WebMvcConfigurer {
         registry.addViewController("/robin").setViewName("success");
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+                .excludePathPatterns("/index.html", "/", "/user/login");
+    }
+
     @Bean
     public WebMvcConfigurer webMvcConfigurer() {
         return new WebMvcConfigurer() {
@@ -21,6 +29,7 @@ public class MyMvcConfig implements WebMvcConfigurer {
             public void addViewControllers(ViewControllerRegistry registry) {
                 registry.addViewController("/").setViewName("login");
                 registry.addViewController("/index.html").setViewName("login");
+                registry.addViewController("/main.html").setViewName("dashboard");
             }
         };
     }
