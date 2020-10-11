e2d606e7d90f523b62c9f2c5ac8bccb379f15186
robin-qu <hongbinqu9@gmail.com>
2020-10-11 18:50:21 +0800

    springboot jdbc

--- springboot-05-data-jdbc/src/test/java/com/robin/springboot05datajdbc/Springboot05DataJdbcApplicationTests.java
+++ springboot-05-data-jdbc/src/test/java/com/robin/springboot05datajdbc/Springboot05DataJdbcApplicationTests.java
@@ -1,13 +1,19 @@
 package com.robin.springboot05datajdbc;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class Springboot05DataJdbcApplicationTests {
+    @Autowired
+    private DataSource dataSource;
 
     @Test
     void contextLoads() {
+        System.out.println(dataSource.getClass());
     }
 
 }
