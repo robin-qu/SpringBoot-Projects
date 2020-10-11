8de7e3d934d308ae54fbd397eb0eb7143c977e18
robin-qu <hongbinqu9@gmail.com>
2020-10-11 18:24:17 +0800

    springboot database

---
+++ springboot-05-data-jdbc/src/test/java/com/robin/springboot05datajdbc/Springboot05DataJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robin.springboot05datajdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot05DataJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
