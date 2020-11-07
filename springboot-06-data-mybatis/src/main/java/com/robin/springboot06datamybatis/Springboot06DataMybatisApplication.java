0a0972e4d7cef31557e0808820756e799ee9169f
robin-qu <hongbinqu9@gmail.com>
2020-11-07 18:55:14 +0800

    springboot mybatis initial commit

---
+++ springboot-06-data-mybatis/src/main/java/com/robin/springboot06datamybatis/Springboot06DataMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.robin.springboot06datamybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot06DataMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot06DataMybatisApplication.class, args);
+    }
+
+}
---
+++ springboot-06-data-mybatis/src/test/java/com/robin/springboot06datamybatis/Springboot06DataMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robin.springboot06datamybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot06DataMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
