f314857e8bfb2849ffb3f4d99b76f5c478eaae9a
robin-qu <hongbinqu9@gmail.com>
2020-11-08 18:29:51 +0800

    springboot mybatis configuration file

--- springboot-06-data-mybatis/src/main/java/com/robin/springboot06datamybatis/controller/DepartmentController.java
+++ springboot-06-data-mybatis/src/main/java/com/robin/springboot06datamybatis/controller/DepartmentController.java
@@ -1,7 +1,9 @@
 package com.robin.springboot06datamybatis.controller;
 
 import com.robin.springboot06datamybatis.bean.Department;
+import com.robin.springboot06datamybatis.bean.Employee;
 import com.robin.springboot06datamybatis.mapper.DepartmentMapper;
+import com.robin.springboot06datamybatis.mapper.EmployeeMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,9 @@ public class DepartmentController {
     @Autowired
     private DepartmentMapper departmentMapper;
 
+    @Autowired
+    private EmployeeMapper employeeMapper;
+
     @GetMapping("/department/{id}")
     public Department getDepartment(@PathVariable("id") Integer id) {
         return departmentMapper.getDepartmentById(id);
@@ -22,5 +27,16 @@ public class DepartmentController {
     public Department insertDepartment(Department department) {
         departmentMapper.insertDepartment(department);
         return department;
+    }
+
+    @GetMapping("/employee/{id}")
+    public Employee getEmployee(@PathVariable("id") Integer id) {
+        return employeeMapper.getEmployee(id);
+    }
+
+    @GetMapping("/employee")
+    public Employee insertEmployee(Employee employee) {
+        employeeMapper.insertEmployee(employee);
+        return employee;
     }
 }
