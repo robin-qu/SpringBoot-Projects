fdf3f3a87ff6ac8ff475cdff416488311d78e62f
robin-qu <hongbinqu9@gmail.com>
2020-11-08 18:52:09 +0800

    springboot data jpa initial commit

---
+++ springboot-06-jpa/src/main/java/com/robin/springboot06jpa/Springboot06JpaApplication.java
@@ -0,0 +1,13 @@
+package com.robin.springboot06jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot06JpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot06JpaApplication.class, args);
+    }
+
+}
---
+++ springboot-06-jpa/src/test/java/com/robin/springboot06jpa/Springboot06JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robin.springboot06jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot06JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
